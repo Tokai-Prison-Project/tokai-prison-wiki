bcc65d0239ce4558d350c7be53b5f449813c119c
Nakatai-0322 <nakatai0322@gmail.com>
2021-11-23 09:04:51 +0900

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,18 +5,3 @@ locales: {
         description: '”東海刑務所”のWiki.',
     }
   },
- themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'About', link: '/about/' },
-      { text: 'Blog', link: '/blog/' }
-    ],
-    sidebar: [
-      {
-        title: 'Blog',
-        children: [
-          '/blog/'
-        ]
-      }
-    ]
-  }
