e07dfa7d4a1cd4ef1a02d4a564659461b3f2fe08
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-08 16:55:19 +0900

    Change Some Config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'Tokai Prison Wiki',
+	description: '',
+	lang: 'ja-jp',
+	head: [],
+	themeConfig: {
+		repo: 'Tokai-Prison-Project/tokai-prison-wiki',
+		docsDir: 'docs',
+		editLinks: true,
+		editLinkText: 'Edit In GitHub',
+		lastUpdated: '更新日',
+		nav: [
+			{ text: 'トップページ', link: '/' },
+		],
+	},
+}
