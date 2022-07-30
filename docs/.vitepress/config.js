e6400a5378720c5075c0d14f4a0d2e06345757bb
Nakatai-0322 <nakatai0322@gmail.com>
2022-07-31 08:46:39 +0900

    init vitepress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,26 +1,14 @@
-import { defineConfig } from 'vitepress'
-
-export default defineConfig({
-    lang: 'ja-JP',
+module.exports = {
     title: 'Tokai Prison Wiki',
-    lastUpdated: true,
-    themeConfig: {
-        nav: [
-            { text: 'トップページ', link: '/' },
-        ],
-
-        editLink: {
-            pattern: 'https://github.com/Tokai-Prison-Project/tokai-prison-wiki/edit/main/docs/:path',
-            text: 'Edit this page on GitHub'
-        },
-
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/Tokai-Prison-Project' }
-        ],
-
-        footer: {
-            message: 'Released under the CC-BY-4.0 License.',
-            copyright: 'Copyright (c) 2022-present Tokai Prison Project'
-        }
-    }
-})
+	lang: 'ja-jp',
+	themeConfig: {
+		repo: 'Tokai-Prison-Project/tokai-prison-wiki',
+		docsDir: 'docs',
+		editLinks: true,
+		editLinkText: 'GitHubで編集',
+		lastUpdated: '更新日',
+		nav: [
+			{ text: 'トップページ', link: '/' },
+		],
+	},
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
 # Tokai Prison Wiki
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/218ca7cd-9a01-4a2c-bfa9-bb8ea1d6efcc/deploy-status)](https://app.netlify.com/sites/tokai-prison-wiki/deploys)
